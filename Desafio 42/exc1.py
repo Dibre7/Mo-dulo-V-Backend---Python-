4e377d0b8b045fe230831a03ec33f22c625b6309
Dibre7 <gabrielgx3@gmail.com>
2024-08-20 18:53:45 -0300

    Add Desafio 42 pasta

---
+++ Desafio 42/exc1.py
@@ -0,0 +1,9 @@
+# Recebendo os valores do usuário
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+
+# Carlculando a soma
+soma = valor1 + valor2
+
+# Exibindo o resultado
+print(f"A soma de {valor1} e {valor2} é {soma}")
